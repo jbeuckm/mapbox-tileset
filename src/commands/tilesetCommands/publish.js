95cf4961e5a932160ceed148bc7406ae7b7ba188
Joseph Beuckman <joebeuckman@gmail.com>
2019-10-25 22:01:32 -0500

    create and publish a tileset

--- src/commands/tilesetCommands/publish.js
+++ src/commands/tilesetCommands/publish.js
@@ -3,26 +3,19 @@ const { MAPBOX_API_HOST } = require('../../constants.json')
 const getCredentials = require('../../getCredentials')
 const querystring = require('querystring')
 
-exports.command = 'create <tileset_id> <recipe>'
-exports.desc = 'Create a tileset'
+exports.command = 'publish <tileset_id>'
+exports.desc = 'Publish a tileset'
 exports.builder = {}
 exports.handler = function(argv) {
   const { username, token } = getCredentials(argv)
 
-  const recipe = require(process.cwd() + '/' + argv.recipe)
-  const body = JSON.stringify(recipe)
-
   const req = request(
     {
       host: MAPBOX_API_HOST,
-      path: `/tilesets/v1/${username}.${argv.tileset_id}?${querystring.stringify({
+      path: `/tilesets/v1/${username}.${argv.tileset_id}/publish?${querystring.stringify({
         access_token: token,
       })}`,
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': body.length,
-      },
     },
     response =>
       response.on('data', data => {
@@ -34,6 +27,5 @@ exports.handler = function(argv) {
     console.error(e)
   })
 
-  req.write(body)
   req.end()
 }
